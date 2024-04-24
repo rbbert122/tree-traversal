e03cf923aab2a882ff1b000f23344e8c49678636
Robert-Iulian Balan <robertbalan930@gmail.com>
2024-04-24 10:55:57 +0300

    Remove tests

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -19,16 +19,6 @@ class TestFindMethod(unittest.TestCase):
         node = self.tree._find(20, self.tree.root)
         self.assertIsNone(node)
 
-    def test_find_existing_node_in_left_subtree(self):
-        node = self.tree._find(5, self.tree.root)
-        self.assertIsNotNone(node)
-        self.assertEqual(node.data, 5)
-
-    def test_find_existing_node_in_right_subtree(self):
-        node = self.tree._find(15, self.tree.root)
-        self.assertIsNotNone(node)
-        self.assertEqual(node.data, 15)
-
 
 if __name__ == "__main__":
     unittest.main()
